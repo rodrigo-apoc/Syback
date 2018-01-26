9a31bd4340c0eb5b7c895d6ceabe02ad1e1c8522
Rodrigo (Apoc) <rodrigof.lops@gmail.com>
2018-01-26 16:24:17 -0200

    Loading...

--- Syback.py
+++ Syback.py
@@ -3,11 +3,23 @@
 import os,time
 from datetime import datetime
 
+
 class Fullmode:
 
     now = time.time()
 
-    ## Function to dele oldest files of retention policy ##
+    ## Function to make backup FULL ##
+    def make(self,dirbkp,user,passw,dbname,server):
+        self.dir = dirbkp
+        self.user = user
+        self.passw = passw
+        self.db = dbname
+        self.server = server
+
+        return os.system('echo isql -U%s -P%s -S%s -D%s -X -i\"%s\" -o\"%s\log_bkp_full.log\"' % (self.user,self.passw,self.server,self.db,self.dir,self.dir))
+
+
+    ## Function to delete oldest files of retention policy ##
     def retention(self,dirbkp,retention):
         self.dir = dirbkp
         self.ret = retention
@@ -19,24 +31,39 @@ class Fullmode:
                 if "log_bkp_full.log" in file:
                         pass 
                 elif last_write > (self.ret * 24):
-                        print("Deleted: %s" % file) ## ONLY FOR TESTS
+                        return "Deleted: %s" % file ## ONLY FOR TESTS
                         ## os.remove(file)
                 else:
                         pass
         return
 
 
-## Funcao para coletar tamanho do arquivo de Bkp principal ##
-#def size_file(dir):
-#        for file in os.listdir(dir):
-#                ## Condicao para coletar tamanho apenas do file principal ##
-#                if bkpfile in file:
-#                        os.chdir(dir)
-#                        size = os.stat(file).st_size/1024/1024
-#                        cdate = time.ctime(os.stat(file).st_ctime)
-#                        print('Tamanho: %.0f Mb' % size) ## apagar ao fim dos 
-#                        d = datetime.strptime(cdate, '%a %b %d %H:%M:%S %Y')
-#                        print(d.strftime('%d/%m/%Y'))
-#                        ## return size
-#                else:
-#                        pass
+    ## Function to colect size of backup ##
+    def size(self,dir,dbname):
+        self.dir = dir
+        self.db = dbname
+
+        for file in os.listdir(self.dir):
+            os.chdir(self.dir)
+            ## Reading size of the {DBNAME} bkp file ##
+            if str(self.db) in file:
+                ## Size in MB ##
+                size = os.stat(file).st_size/1024/1024
+
+                return 'Size: %.0f Mb' % size
+        return
+
+
+    ## Function to colect Datetime of the last backup ##
+    def bkpdate(self,dir,dbname):
+        self.dir = dir
+        self.db = dbname
+
+        for file in os.listdir(self.dir):
+            os.chdir(self.dir)
+            ## Reading creation date of the {DBNAME} bkp file ##
+            if str(self.db) in file:
+                cdate = time.ctime(os.stat(file).st_ctime)
+                return datetime.strptime(cdate, '%a %b %d %H:%M:%S %Y').strftime('%d/%m/%Y')
+        return
+
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Syback import *
 full = Fullmode()
 
 ## Cleaning terminal ##
-subprocess.call('cls', shell=True)
+subprocess.call('clear', shell=True)
 
 ## Help menu ##
 parser = argparse.ArgumentParser(prog='Syback',formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -23,34 +23,50 @@ parser.add_argument('-T','--TLOG',dest='tlog_opt',action='store_true',help='Back
 parser.add_argument('-U','--USER',dest='user_arg',required=True,help='Specify a user to connect on Sybase ASE. The user must have SA role. e.g: -U sapsa')
 parser.add_argument('-P','--PASS',dest='pass_arg',required=True,help='Specify the password of the user.')
 parser.add_argument('-DB',dest='db_arg',required=True,help='Specify the Database name to connect.')
+parser.add_argument('-S',dest='srv_arg',required=True,help='Specify the Server name (..SYBASE_HOME\sql.ini) to connect.')
 parser.add_argument('-R',dest='retb_arg',type=int,help='Specify retention policy in days. Write 0 to no retention policy. This option will delete oldest files BEFORE the backup. To apply retention policy after backup please choose -r instead of -R.')
 parser.add_argument('-r',dest='reta_arg',type=int,help='Specify retention policy in days. Write 0 to no retention policy. This option will delete oldest files AFTER the backup. To apply retention policy before the backup please choose -R instead of -r.')
 
 args = parser.parse_args()
 
+
+## Function to check if dir is a UNC path ##
+def checkdir(dir):
+	if str(args.DIR[0]) == "\\":
+		Ndir = "\\"+"\\"+args.DIR[1:len(args.DIR)]
+		return str(Ndir)
+	else:
+		return str(dir)
+
+
 ## Checking if user choosed a backup mode FULL or TLOG ##
-if str(args.full_opt) == "False" and str(args.tlog_opt) == "False":
+if args.full_opt == False and args.tlog_opt == False:
 	print("You need specify a backup mode [option] on command-line. See syback.py -h for further details.")
 	sys.exit()
-
-## Applying retention policy before to execute backup ##
-if str(args.retb_arg) != "None":
-	full.retention(args.DIR,args.retb_arg)
-	print("Starting backup...") ## ONLY FOR TESTS
-	sys.exit()
-
-## Applying retention policy after execute backup ##
-if str(args.reta_arg) != "None":
-	print("Starting backup..."+'\n'+"Backup finish.") ## ONLY FOR TESTS
-	full.retention(args.DIR,args.reta_arg)
-	sys.exit()
-
-#############################
-######## RODANDO BKP ########
-#############################
-#del_file(dir_bkp,retencao) ## Apagando bkps antigos (retencao) ##
-#os.chdir('C:\\') ## Bypass do erro de UNC invalido devido ao bkp estar em REDE. Nao e necessario essa linha caso o bkp esteja local ##
-#os.system('isql -Usapsa -Ph4tlevS@P -SFID -DFID -X -i\"\\\StoreOnce\DB_Sybase\FIORI\DEV\scripts\BACKUP_FULL.sql\" -o\"\\\StoreOnce\DB_Sybase\FIORI\DEV\DATA\log_bkp_full.log\"')
-#size_file(dir_bkp) ## Coletando tamanho do bkp ##
+## Executing backup FULL ##
+elif args.full_opt == True:
+	## Applying retention policy before to execute backup ##
+	if str(args.retb_arg) != "None":
+		print(full.retention(checkdir(args.DIR),args.retb_arg))
+		full.make(checkdir(args.DIR),args.user_arg,args.pass_arg,args.db_arg,args.srv_arg)
+		print(full.size(checkdir(args.DIR),args.db_arg))
+		print(full.bkpdate(checkdir(args.DIR),args.db_arg))
+		sys.exit()
+	## Applying retention policy after execute backup ##
+	elif str(args.reta_arg) != "None":
+		full.make(checkdir(args.DIR),args.user_arg,args.pass_arg,args.db_arg,args.srv_arg)
+		print(full.size(checkdir(args.DIR),args.db_arg))
+		print(full.bkpdate(checkdir(args.DIR),args.db_arg))
+		print(full.retention(checkdir(args.DIR),args.reta_arg))
+		sys.exit()
+	## Backup without retention policy ##
+	else:
+		full.make(checkdir(args.DIR),args.user_arg,args.pass_arg,args.db_arg,args.srv_arg)
+		print(full.size(checkdir(args.DIR),args.db_arg))
+		print(full.bkpdate(checkdir(args.DIR),args.db_arg))
+		sys.exit()
+## Executing backup TLOG ##
+else:
+	print("Tlog")
 
 #os.system('powershell.exe -command "Start-Sleep -s 100"')
