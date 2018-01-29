0cd28ed18b37556d076351aa2f2d9c0ef362a10c
Rodrigo (Apoc) <rodrigof.lops@gmail.com>
2018-01-29 00:26:27 -0200

    Loading...

--- config.py
+++ config.py
@@ -3,53 +3,93 @@
 import argparse,textwrap
 from appJar import gui
 
-## Details ##
-__author__ = "Rodrigo Lopes - Hele"
-__copyright__ = "Copyright 2018, Hele"
-#__credits__ = [""]
-#__license__ = "Apache 2.0"
-__version__ = "0.1"
-__maintainer__ = "Rodrigo Lopes"
-__email__ = "rodrigof.lops@gmail.com"
-__status__ = "Development"
-#__url__ = ""
+class Helpmenu:
 
-## Help menu ##
-parser = argparse.ArgumentParser(prog='Syback',formatter_class=argparse.RawDescriptionHelpFormatter,
-	description='A Open-source tool for backup Sybase ASE on Windows.',usage=textwrap.dedent('''syback.py [DIR] [option]
-		e.g: syback.py "C:\\Mybkp\dir\location" -F'''),
-	epilog='''Author: '''+__author__+
-	'''\nEmail contact: '''+__email__+
-	'''\nVersion: '''+__version__+
-	'''\nSource: https://github.com/rodrigo-apoc/Syback\n'''+__copyright__)
-parser.add_argument('DIR',help='Specify the backup directory with double quotes.')
-parser.add_argument('-F','--FULL',dest='full_opt',action='store_true',help='Backup Full mode ON.')
-parser.add_argument('-T','--TLOG',dest='tlog_opt',action='store_true',help='Backup TLog mode ON.')
-parser.add_argument('-U','--USER',dest='user_arg',required=True,help='Specify a user to connect on Sybase ASE. The user must have SA role. e.g: -U sapsa')
-parser.add_argument('-P','--PASS',dest='pass_arg',required=True,help='Specify the password of the user.')
-parser.add_argument('-DB',dest='db_arg',required=True,help='Specify the Database name to connect.')
-parser.add_argument('-S',dest='srv_arg',required=True,help='Specify the Server name (..SYBASE_HOME\sql.ini) to connect.')
-parser.add_argument('-R',dest='retb_arg',type=int,help='Specify retention policy in days. Write 0 to no retention policy. This option will delete oldest files BEFORE the backup. To apply retention policy after backup please choose -r instead of -R.')
-parser.add_argument('-r',dest='reta_arg',type=int,help='Specify retention policy in days. Write 0 to no retention policy. This option will delete oldest files AFTER the backup. To apply retention policy before the backup please choose -R instead of -r.')
-
-args = parser.parse_args()
+	def menu(self):
+		## Details ##
+		__author__ = "Rodrigo Lopes"
+		__copyright__ = "Copyright 2018, Rodrigo Lopes"
+		#__credits__ = [""]
+		#__license__ = "Apache 2.0"
+		__version__ = "0.1"
+		__maintainer__ = "Rodrigo Lopes"
+		__email__ = "rodrigof.lops@gmail.com"
+		__status__ = "Development"
+		#__url__ = ""
 		
+		
+		## Help menu ##
+		parser = argparse.ArgumentParser(prog='Syback',formatter_class=argparse.RawDescriptionHelpFormatter,
+			description='A Open-source tool for backup Sybase ASE on Windows.',usage=textwrap.dedent('''syback.py [DIR] [option] e.g: syback.py "C:\\Mybkp\dir\location" -F'''),
+			epilog='''Author: '''+__author__+
+			'''\nEmail contact: '''+__email__+
+			'''\nVersion: '''+__version__+
+			'''\nSource: https://github.com/rodrigo-apoc/Syback\n'''+__copyright__)
+		parser.add_argument('-F','--FULL',dest='full_opt',action='store_true',help='Backup Full mode ON.')
+		parser.add_argument('-T','--TLOG',dest='tlog_opt',action='store_true',help='Backup TLog mode ON.')
+		parser.add_argument('--conf',dest='conf_arg',required=True,action='store_true',help='Open settings window to configure backup.')
 
-app = gui(handleArgs=False)
+		#parser.add_argument('DIR',help='Specify the backup directory with double quotes.')
+		#parser.add_argument('-F','--FULL',dest='full_opt',action='store_true',help='Backup Full mode ON.')
+		#parser.add_argument('-T','--TLOG',dest='tlog_opt',action='store_true',help='Backup TLog mode ON.')
+		#parser.add_argument('-U','--USER',dest='user_arg',help='Specify a user to connect on Sybase ASE. The user must have SA role. e.g: -U sapsa')
+		#parser.add_argument('-P','--PASS',dest='pass_arg',help='Specify the password of the user.')
+		#parser.add_argument('-DB',dest='db_arg',help='Specify the Database name to connect.')
+		#parser.add_argument('-S',dest='srv_arg',help='Specify the Server name (..SYBASE_HOME\sql.ini) to connect.')
+		#parser.add_argument('-R',dest='retb_arg',type=int,help='Specify retention policy in days. Write 0 to no retention policy. This option will delete oldest files BEFORE the backup. To apply retention policy after backup please choose -r instead of -R.')
+		#parser.add_argument('-r',dest='reta_arg',type=int,help='Specify retention policy in days. Write 0 to no retention policy. This option will delete oldest files AFTER the backup. To apply retention policy before the backup please choose -R instead of -r.')
+		#parser.add_argument('--conf',dest='conf_arg',required=True,action='store_true',help='Open settings window to configure backup.')
+		
+		args = parser.parse_args()
 
-## Layout config ##
-app.setTitle("Config Window")
-app.setSize("fullscreen")
+		return args
 
-## Header ##
-app.addLabel("header","Settings page of Syback. All informations will be saved on instalation directory.",0,0,3)
 
-## Informations ##
-app.addLabelEntry("Username: ")
+class Settings:
 
-## GRID VIEW ##
-app.setLabelBg("header","red")
-app.getLabelWidget("header").config(font="Helvetica 22")
-app.setLabelBg("Username: ","blue")
+	def window(self):
 
-app.go()
+		def retention(btn):
+			app.addLabelOptionBox("Delete", ["After", "Before"], 5, 5)
+			app.addLabel("txt1Lab", "backup.", 5, 6)
+			app.addEntry("daysEntry", 5, 7, 0, 0)
+			app.addLabel("txt2Lab", "day(s).", 5, 8)
+
+		## GUI ##
+		app = gui(handleArgs=False,title="Settings Window")#,size="fullscreen")
+		app.setBg("lightBlue")
+
+		## Header ##
+		#app.addLabel("header","Settings page of Syback. All informations will be saved on instalation directory.",0,0,3)
+
+		## First Block ##
+		app.addLabel("title1Lab", "Connection", 0, 0, 9)
+		app.addHorizontalSeparator(1,0,9)
+		app.addLabel("userLab", "Username:", 2, 0)
+		app.addEntry("userEntry", 2, 1)
+		app.addLabel("passLab", "Password:", 2, 2)
+		app.addSecretEntry("passEntry", 2, 3)
+		app.addLabel("dbLab", "DBName:", 2, 4)
+		app.addEntry("dbEntry", 2, 5)
+		app.addLabel("srvLab", "Server:", 2, 6)
+		app.addEntry("srvEntry", 2, 7)
+		app.addCheckBox("Encryption", 2, 8)
+
+		## Second BLock ##
+		app.addLabel("title2Lab", "Backup and Retention", 3, 0, 9)
+		app.addHorizontalSeparator(4,0,9)
+		app.addLabel("dirLab", "Backup Directory:", 5, 0)
+		app.addEntry("dirEntry", 5, 1, 3)
+		app.addButton("Enable retention policy", retention, 5, 4)
+
+		## Adjustments ##
+		app.setFocus("userEntry")
+		app.getLabelWidget("title1Lab").config(font="Helvetica 18")
+		app.getLabelWidget("title2Lab").config(font="Helvetica 18")
+
+		## GRID VIEW ##
+		#app.setLabelBg("header","red")
+		#app.getLabelWidget("header").config(font="Helvetica 22")
+
+		app.go()
+
--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from config import *
 
 ## Creating class instance ##
 full = Fullmode()
-hh = Help()
-
-print(hh.menu())
+helpmenu = Helpmenu()
+conf = Settings()
 
 ## Cleaning terminal ##
 subprocess.call('clear', shell=True)
 
+## Calling help menu ##
+help = helpmenu.menu()
 
 ## Function to check if dir is a UNC path ##
 def checkdir(dir):
@@ -25,34 +26,38 @@ def checkdir(dir):
 		return str(dir)
 
 
-## Checking if user choosed a backup mode FULL or TLOG ##
-if args.full_opt == False and args.tlog_opt == False:
-	print("You need specify a backup mode [option] on command-line. See syback.py -h for further details.")
-	sys.exit()
-## Executing backup FULL ##
-elif args.full_opt == True:
-	## Applying retention policy before to execute backup ##
-	if str(args.retb_arg) != "None":
-		print(full.retention(checkdir(args.DIR),args.retb_arg))
-		full.make(checkdir(args.DIR),args.user_arg,args.pass_arg,args.db_arg,args.srv_arg)
-		print(full.size(checkdir(args.DIR),args.db_arg))
-		print(full.bkpdate(checkdir(args.DIR),args.db_arg))
-		sys.exit()
-	## Applying retention policy after execute backup ##
-	elif str(args.reta_arg) != "None":
-		full.make(checkdir(args.DIR),args.user_arg,args.pass_arg,args.db_arg,args.srv_arg)
-		print(full.size(checkdir(args.DIR),args.db_arg))
-		print(full.bkpdate(checkdir(args.DIR),args.db_arg))
-		print(full.retention(checkdir(args.DIR),args.reta_arg))
-		sys.exit()
-	## Backup without retention policy ##
-	else:
-		full.make(checkdir(args.DIR),args.user_arg,args.pass_arg,args.db_arg,args.srv_arg)
-		print(full.size(checkdir(args.DIR),args.db_arg))
-		print(full.bkpdate(checkdir(args.DIR),args.db_arg))
-		sys.exit()
-## Executing backup TLOG ##
-else:
-	print("Tlog")
+## Calling settings window ##
+if help.conf_arg == True:
+	conf.window()
 
+## Executing backup FULL ##
+#if help.full_opt == True:
+	## Applying retention policy before to execute backup ##
+#	if str(args.retb_arg) != "None":
+#		print(full.retention(checkdir(args.DIR),args.retb_arg))
+#		full.make(checkdir(args.DIR),args.user_arg,args.pass_arg,args.db_arg,args.srv_arg)
+#		print(full.size(checkdir(args.DIR),args.db_arg))
+#		print(full.bkpdate(checkdir(args.DIR),args.db_arg))
+#		sys.exit()
+#	## Applying retention policy after execute backup ##
+#	elif str(args.reta_arg) != "None":
+#		full.make(checkdir(args.DIR),args.user_arg,args.pass_arg,args.db_arg,args.srv_arg)
+#		print(full.size(checkdir(args.DIR),args.db_arg))
+#		print(full.bkpdate(checkdir(args.DIR),args.db_arg))
+#		print(full.retention(checkdir(args.DIR),args.reta_arg))
+#		sys.exit()
+#	## Backup without retention policy ##
+#	else:
+#		full.make(checkdir(args.DIR),args.user_arg,args.pass_arg,args.db_arg,args.srv_arg)
+#		print(full.size(checkdir(args.DIR),args.db_arg))
+#		print(full.bkpdate(checkdir(args.DIR),args.db_arg))
+#		sys.exit()
+### Executing backup TLOG ##
+#else:
+#	print("Tlog")
+
+
+#if args.full_opt == False and args.tlog_opt == False:
+#	print("You need specify a backup mode [option] on command-line. See syback.py -h for further details.")
+#	sys.exit()
 #os.system('powershell.exe -command "Start-Sleep -s 100"')
